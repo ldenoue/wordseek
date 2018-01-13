f8b34a69b4b66d80e04cc2971165512578c3a0e2
Laurent Denoue <denoue@fxpal.com>
2018-01-13 16:07:15 +0100

    new app

---
+++ crossword-ios/com.appblit.crossword/com.appblit.crossword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  com.appblit.crossword
+//
+//  Created by Laurent Denoue on 1/13/18.
+//  Copyright © 2018 Laurent Denoue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ crossword-ios/com.appblit.crossword/com.appblit.crossword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  com.appblit.crossword
+//
+//  Created by Laurent Denoue on 1/13/18.
+//  Copyright © 2018 Laurent Denoue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
